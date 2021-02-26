cebe428da7bd2cbf521a9beaa69ca817871fd373
Diya Wadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2021-02-26 19:35:41 +0300

    Update SubarrayWithGivenSum.c

--- SubarrayWithGivenSum.c
+++ SubarrayWithGivenSum.c
@@ -1,3 +1,15 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find the subarray with given sum k
+// arr: input array
+// n: size of array
+//answer
 vector<int> subarraySum(int arr[], int n, int s){
     
     int sum=0, j;
@@ -30,3 +42,32 @@ vector<int> subarraySum(int arr[], int n, int s){
     
     return newArr;
 }
+
+
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        long long s;
+        cin>>n>>s;
+        int arr[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        
+        vector<int>res;
+        res = subarraySum(arr, n, s);
+        
+        for(int i = 0;i<res.size();i++)
+            cout<<res[i]<<" ";
+        cout<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
