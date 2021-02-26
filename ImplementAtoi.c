ded9c8a82cb11e28c1239a556bc78abe7958ab16
Diya Wadhwani <54880903+DiyaWadhwani@users.noreply.github.com>
2021-02-26 20:28:02 +0300

    Create ImplementAtoi.c

---
+++ ImplementAtoi.c
@@ -0,0 +1,45 @@
+// { Driver Code Starts
+ #include <bits/stdc++.h>
+using namespace std;
+
+int atoi(string str);
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		cout<<atoi(s)<<endl;
+	}
+}// } Driver Code Ends
+
+
+//answer
+/*You are required to complete this method */
+int atoi(string str)
+{
+    
+    int n = str.length();
+    int integer=0, i;
+    string val;
+   
+    for(i=0; i<n; i++)
+    {
+        if(str[i]=='-' && isdigit(str[i+1]))
+            val.push_back(str[i]);
+        if(isdigit(str[i]))
+            val.push_back(str[i]);
+        else if(isalpha(str[i]))
+        {
+            return -1;
+            break;
+        }
+    }
+    
+  //convert string to int
+    stringstream final(val);
+    final>>integer;
+    return integer;
+}
